d58340e836103dea0589bd6d3e81a6e7ec997b0c
Aswin0512 <aswinrajan0512@gmail.com>
2026-02-18 17:44:21 +0530

    modified:   Plot.py

--- Plot.py
+++ Plot.py
@@ -1,10 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D 
 import math as ma
 
 
-Kx=np.linspace(-np.pi,np.pi,1000)
-Ky=np.linspace(-np.pi,np.pi,1000)
-Kz=np.linspace(-np.pi,np.pi,1000)
+x=np.linspace(-np.pi,np.pi,10)
+y=np.linspace(-np.pi,np.pi,10)
+z=np.linspace(-np.pi,np.pi,10)
+#creating a 3d mesh
+kx,ky=np.meshgrid(x,y)
+
+kxm=np.ones((10,10,10))
+kym=np.ones((10,10,10))
+kzm=np.ones((10,10,10))
+
+for i in range(10):
+    kxm[i,:,:]=kx
+    kym[i,:,:]=ky
+
+    kzz=z[i]*np.ones((10,10))
+    kzm[i,:,:]=kzz
+
 
 E=(Kx**2+Ky**2+Kz**2)/2
+
+fig=plt.figure(1)
+ax=fig.add_subplot(projection='3d')
+Energy=ax.scatter(Kx,Ky,Kz,c=E,cmap="viridis")
+plt.colorbar(Energy)
+plt.show()
