db919acf6077874f9631e0590721f4dda35078cb
Aswin0512 <aswinrajan0512@gmail.com>
2026-02-20 13:56:15 +0530

    modified:   Plot.py

--- Plot.py
+++ Plot.py
@@ -3,17 +3,24 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D 
 import math as ma
 
-
-x=np.linspace(-np.pi,np.pi,10)
-y=np.linspace(-np.pi,np.pi,10)
-z=np.linspace(-np.pi,np.pi,10)
+l=25
+Ev=1
+x=np.linspace(-np.pi,np.pi,l)
+y=np.linspace(-np.pi,np.pi,l)
+z=np.linspace(-np.pi,np.pi,l)
 #creating a 3d mesh
 kx,ky,kz=np.meshgrid(x,y,z)
+m=0
+#float E = 0 
+for i in range(l):
+    for j in range(l):
+        for k in range(l):
+            #if E=(kx[i,j,k]**2+ky[i,j,k]**2+kz[i,j,k]**2)/2 - Ev <= 0.0001:
 
-E=(kx**2+ky**2+kz**2)/2
+#E=(kx**2+ky**2+kz**2)/2
 
 fig=plt.figure()
 ax=fig.add_subplot(projection='3d')
-Energy=ax.scatter3D(kx,ky,kz)
-#plt.colorbar(Energy)
+Energy=ax.scatter3D(np.ravel(kx),np.ravel(ky),np.ravel(kz),c=np.ravel(E),cmap='viridis')
+plt.colorbar(Energy)
 plt.show()
