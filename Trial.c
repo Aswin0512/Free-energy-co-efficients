8eeef0db0f9408b2ff6d7a0c5eb0a36e6a8aeab2
Aswin0512 <aswinrajan0512@gmail.com>
2026-02-27 20:17:21 +0530

    new file:   Trial.c 	modified:   fermisurface average.c

---
+++ Trial.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<math.h>
+
+//integral of sinx
+int main(){
+
+}
--- fermisurface average.c
+++ fermisurface average.c
@@ -3,6 +3,18 @@
 
 //To calculate the fermi surface average of different functions
 
+// functon inside integral
+float fun(float kx,float ky){
+    float f=1/(sqrt(kx*kx+ky*ky));
+    return f;
+}
+
+//function to find the points on a fermi curve
+float surface(float x[],float y[],){
+    
+}
+
+
 int main(){
 
 }
