21f369197e68fcd2650de7fd17839016fa24bc56
Aswin0512 <aswinrajan0512@gmail.com>
2026-03-03 09:53:21 +0530

    modified:   Trial.c 	modified:   a.out 	modified:   fermisurface average.c

--- Trial.c
+++ Trial.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdlib.h>
+#define PI 3.141592
 
 //integral of sinx
 int main(){
-    float pi=M_PI;
+    float pi=PI;
+    pi=abs(pi);
     printf("%f\n",pi);
 }
--- fermisurface average.c
+++ fermisurface average.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdlib.h>
+#define PI 3.141592
 
 //To calculate the fermi surface average of different functions
 
@@ -17,7 +19,26 @@ float MdelEnergy(float kx,float ky){
 //function to find the points on a fermi curve
 float surface(float Ev,float Etol,int n){
     float kstep=Etol/sqrt(2*Ev);
-    float thetastep=
+    float thetastep=PI/(4*(n-1));
+    float E=0;
+    float Etolc=1;
+    float k=0;
+    float theta=0;
+    float kx=0;
+    float ky=0;
+    for (int i = 0; i < n; i++)
+    {
+        theta=i*thetastep;
+        k=0;
+        while (Etolc<=Etol)
+        {   
+            kx=k*cos(theta);
+            ky=k*sin(theta);
+            E=Energy(kx,ky);
+            Etolc=fabs(E-Ev); 
+        }
+    }
+    
 }
 
 
